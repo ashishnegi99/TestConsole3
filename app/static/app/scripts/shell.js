e0c2c116f21d52dd34535414d01100209fe2f705
Sidhartha Saurav <Sidhartha@VerizonHcl.com>
2017-03-07 10:58:08 -0600

    @Sathya menu icon and down arrow bug

--- app/static/app/scripts/shell.js
+++ app/static/app/scripts/shell.js
@@ -3,7 +3,9 @@ $(document).ready(function() {
 						"menu-0": {
 							"id": "menu-0",
 							"title": "Home",
-							"icon": "glyphicon glyphicon-home",
+							
+							"icon": "/static/app/images/home.png",
+							
 							"parentId": null,
 							"url": "http://127.0.0.1:8000/",
 							"childCount": 0,
@@ -12,7 +14,8 @@ $(document).ready(function() {
 						"menu-1": {
 							"id": "menu-1",
 							"title": "Reports",
-							"icon": "glyphicon glyphicon-list-alt",
+						
+							"icon": "/static/app/images/reports.png",
 							"parentId": null,
 							"url": "/Reports",
 							"childCount": 0,
@@ -21,7 +24,8 @@ $(document).ready(function() {
 						"menu-5": {
 							"id": "menu-5",
 							"title": "Revo",
-							"icon": "glyphicon glyphicon-th",
+							
+							"icon": "/static/app/images/revo.png",
 							"parentId": null,
 							"url": "/revo",
 							"childCount": 1,
@@ -30,7 +34,8 @@ $(document).ready(function() {
 						"menu-4": {
 							"id": "menu-4",
 							"title": "Configuration",
-							"icon": "glyphicon glyphicon-cog",
+							
+							"icon": "/static/app/images/config.png",
 							"parentId": "menu-5",
 							"url": "",
 							"childCount": 4,
@@ -75,7 +80,8 @@ $(document).ready(function() {
 						"menu-10": {
 							"id": "menu-10",
 							"title": "Storm",
-							"icon": "glyphicon glyphicon-picture",
+							
+							"icon": "/static/app/images/storm.png",
 							"parentId": null,
 							"url": "/Storm",
 							"childCount": 0,
@@ -84,7 +90,8 @@ $(document).ready(function() {
 						"menu-11": {
 							"id": "menu-11",
 							"title": "Appium",
-							"icon": "glyphicon glyphicon-check",
+							
+							"icon": "/static/app/images/appium.png",
 							"parentId": null,
 							"url": "/Appium",
 							"childCount": 0,
@@ -93,33 +100,41 @@ $(document).ready(function() {
 					};
 					
 	function recurseMenu(parent) {
+		
 		var flag = 0;
+		
 		if(parent == undefined){
+		
 			var s = '<ul>';
 		}else if(parent != undefined){
+			
 			var s = '<ul class="submenu">';			
 		}
 		for (var x in menuItems) {
+			
 			var url2 = menuItems[x].title;
 			if (menuItems[x].parentId == parent) {
 				if(menuItems[x].childCount > 1){ 
+				
 					s +='<div class="accordion-body panel-collapse collapse scroller" role="tab" id="RevoCollapse">';
 					s +='<li class="'+ menuItems[x].classname+'"><div class="accordion-heading">';
-					s +='<i role="button" data-toggle="collapse" data-parent="#accordion2" href="#ConfigCollapse" aria-expanded="false" class="collapsed more-less glyphicon glyphicon-large glyphicon-chevron-right config-open" id="revo_openclose2" title="Configuration"></i>'
-					s +='<a title="'+ menuItems[x].title+'" class="config_link">' + menuItems[x].title +'</a>';
+					s +='<i role="button" data-toggle="collapse" data-parent="#accordion2" href="#ConfigCollapse" aria-expanded="false" class="more-less collapsed glyphicon glyphicon-large glyphicon-chevron-right config-open" id="revo_openclose2" title="Configuration"></i>'
+					s +='<img src="'+ menuItems[x].icon+'" alt="'+ menuItems[x].classname+'" class="img-responsive menu_icon"><a title="'+ menuItems[x].title+'" class="config_link">' + menuItems[x].title +'</a>';
 					s += recurseMenu(menuItems[x].id);
 					s += '</div></li></div><hr>';	
 				}
 				else if(menuItems[x].childCount == 1){
+					
 					s +='<div class="accordion" id="accordion2"><li class="'+ menuItems[x].classname+'"> <div class="accordion-heading">';
 					s +='<i role="button" data-toggle="collapse" data-parent="#accordion2" href="#RevoCollapse" aria-expanded="false" class="more-less glyphicon glyphicon-large glyphicon-chevron-right config-open" id="revo_openclose" title="Configuration"></i>';
-					s +='<a href="'+ menuItems[x].url +'" title="'+ menuItems[x].title+'"class="revo_link">' + menuItems[x].title +'</a>';
+					s +='<img src="'+ menuItems[x].icon+'" alt="'+ menuItems[x].classname+'" class="img-responsive menu_icon2"><a href="'+ menuItems[x].url +'" title="'+ menuItems[x].title+'"class="revo_link">' + menuItems[x].title +'</a>';
 					s += recurseMenu(menuItems[x].id);
 					s += '</div></li><hr></div>';
 				}
 				else{
 					if(parent == undefined){
-						s += '<li class="'+ menuItems[x].classname+'"><a href="'+ menuItems[x].url +'" title="'+ menuItems[x].title+'" >' + menuItems[x].title +'</a>';
+					
+						s += '<li class="'+ menuItems[x].classname+'"><img src="'+ menuItems[x].icon+'" alt="'+ menuItems[x].classname+'" class="img-responsive menu_icon2"><a href="'+ menuItems[x].url +'" title="'+ menuItems[x].title+'" >' + menuItems[x].title +'</a>';
 						s += '</li><hr>';
 					}
 					else{
@@ -127,7 +142,7 @@ $(document).ready(function() {
 							s +='<div class="accordion-body panel-collapse collapse scroller" role="tab" id="ConfigCollapse">';
 						}
 						if(menuItems[x].parentId != null){
-							s += '<li class="'+ menuItems[x].classname+'"><a href="'+ menuItems[x].url +'" title="'+ menuItems[x].title+'" ><span class="'+ menuItems[x].icon+'"></span>' + menuItems[x].title +'</a>';
+							s += '<li class="'+ menuItems[x].classname+'"><a href="'+ menuItems[x].url +'" title="'+ menuItems[x].title+'" >' + menuItems[x].title +'</a>';
 							s += '</li><hr>';
 							flag ++;
 						}
@@ -144,15 +159,29 @@ $(document).ready(function() {
 	
 	var link=window.location.href.toString().split(window.location.host);	
 	var activeUrl=link[1].split('/');
-	if(activeUrl[1] =="" ){
-		activeUrl[1] = "home";
-		$("."+activeUrl[1]).addClass('active-colors');
-	}
-	if(activeUrl[2] == ""){
-		$("."+activeUrl[1]).addClass('active-colors');
+	var activeUrl2=window.location.search;
+	
+	var subpath = activeUrl[1].split("?");
+	
+	if(subpath[0] =="" ){
+		
+		subpath[0] = "home";
+		
+		$("."+subpath[0]).addClass('active-colors');
 	}
 	else{
-		$("."+activeUrl[1]).addClass('active-colors');
+		$("."+subpath[0]).addClass('active-colors');
+	}
+	
+	
+	
+	if(!activeUrl[2]){
+		
+		$("."+subpath[0]).addClass('active-colors');
+	}
+	else{
+		$("."+subpath[0]).addClass('active-colors');
+		
 		$("."+activeUrl[2]).addClass('active-colors');
 	}
 		
@@ -183,12 +212,12 @@ $(document).ready(function() {
 	
 	$('#RevoCollapse').on('shown.bs.collapse', function () {
        $("#revo_openclose").removeClass("glyphicon-chevron-right").addClass("glyphicon-chevron-down");
+	   
     });
-    $('#RevoCollapse').on('hidden.bs.collapse', function () {
-       $("#revo_openclose").removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-right");
-    });
+   
 	$('#ConfigCollapse').on('shown.bs.collapse', function () {
        $("#revo_openclose2").removeClass("glyphicon-chevron-right").addClass("glyphicon-chevron-down");
+	   
     });
     $('#ConfigCollapse').on('hidden.bs.collapse', function () {
        $("#revo_openclose2").removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-right");
@@ -198,12 +227,13 @@ $(document).ready(function() {
 		$("#revo_openclose").removeClass("glyphicon-chevron-right").addClass("glyphicon-chevron-down");
 		$("#revo_openclose2").removeClass("glyphicon-chevron-right").addClass("glyphicon-chevron-down");	   
     };
-	function openNav() {
-		document.getElementById("mySidenav").style.width = "250px";
-		/*document.getElementById("main").style.marginLeft = "250px";*/
-	}
-	function closeNav() {
-		document.getElementById("mySidenav").style.width = "0";
-		document.getElementById("main").style.marginLeft= "0";
-	}
+	
+	$(function(){
+		$('#revo_openclose').click(function(){
+			if ( $('#revo_openclose').hasClass('glyphicon-chevron-down') ) {
+				$("#revo_openclose").removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-right");
+			};
+		});
+	});
+	
 });
